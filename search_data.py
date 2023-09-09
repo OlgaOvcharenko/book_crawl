9b689d73abb9c2caee8750fe232523ba768a244a
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-09-09 22:59:46 +0200

    Cache data

--- search_data.py
+++ search_data.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import time
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from enum import Enum
@@ -28,8 +29,12 @@ class BookSearchScraper():
     BOOK_SHOP_BASE_URL = "http://books.toscrape.com"
 
 
-    def __init__(self) -> None:
+    def __init__(self, cache_update_ts: float = 0.0) -> None:
         self.matcher = Matcher()
+        # self.categories_cache = [] can be added if data does not fit in memory
+        self.data_cache = []
+        self.cache_ts = 0.0
+        self.cache_update_ts = cache_update_ts
     
     def preprocess_text(self, text: str):
         return text.lower()
@@ -38,21 +43,35 @@ class BookSearchScraper():
         # Preprocess
         query = self.preprocess_text(query)
 
-        # Get main URL
-        soup_main = self._get_data_from_url()
+        if len(self.data_cache) > 0 and time.time() - self.cache_ts < self.cache_update_ts:
+            final_res = [book for book in self.data_cache if self.matcher.contains_query(query, book[0])]
 
-        # Get categories and get books for each in parallel
-        category_link = self._get_all_category_links(soup_main)
-        final_res = Parallel(n_jobs=len(category_link))(delayed(self._search_in_category)(link, query, extended_info, search_all_pages) for link in category_link.values())
-        final_res = list(itertools.chain.from_iterable(final_res))
+        else:
+            # Get main URL
+            soup_main = self._get_data_from_url()
+
+            # Get categories and get books for each in parallel
+            categories = self._get_all_category_links(soup_main)
+
+            combined_res = Parallel(n_jobs=len(categories))\
+                (delayed(self._search_in_category)\
+                (link, query, extended_info, search_all_pages) for link in categories.values())
+
+            # Merge results
+            final_res, self.data_cache = [], []
+            for vals in combined_res:
+                final_res.extend(vals[0])
+                self.data_cache.extend(vals[1])
+
+            self.cache_ts=time.time()
 
         return final_res
 
     def _search_in_category(self, link: str, query: str, extended_info: bool = False, search_all_pages: bool = False):
         full_url = self.BOOK_SHOP_BASE_URL + "/" + link
         soup_category = self._get_data_from_url(full_url)
-        res_category = self._get_items_from_page_markup(soup_category, query, extended_info, search_all_pages)
-        return res_category
+        res_category, res_cache = self._get_items_from_page_markup(soup_category, query, extended_info, search_all_pages)
+        return res_category, res_cache
 
     def _get_data_from_url(self, url: str = BOOK_SHOP_URL) -> BeautifulSoup:
         """"Extract data from the given url."""
@@ -82,9 +101,11 @@ class BookSearchScraper():
         return url, [name, price]
 
     def _get_items_from_page_markup(self, soup: BeautifulSoup, search_query: str, category_url: str, extended_info: bool = False, search_all_pages: bool = False):
-        result = []
+        # FIXME Cache meaningful?
+        result, cache = [], []
         for book_class in soup.findAll('article', attrs={"class": "product_pod"}):
             new_book_url, new_book = self._get_book_info_from_page(book_class, extended_info)
+            cache.append(new_book)
             if self.matcher.contains_query(search_query, new_book[0]):
                 result.append(new_book)
 
@@ -95,7 +116,7 @@ class BookSearchScraper():
                 next_soap = self._get_data_from_url(category_url.replace("index.html", "") + "/" + next_page)
                 self._get_items_from_page_markup(next_soap, search_query, category_url)
         
-        return result
+        return result, cache
 
     def _get_all_category_links(self, soup: BeautifulSoup):
         all_generes = soup.find('ul', attrs={"class": "nav nav-list"}).findAll("a", href=True)
@@ -129,7 +150,7 @@ def print_matching_result(query: str, matches: list):
         (max([len(str(row[i])) for row in matches]) + 4) for i in range(len(matches[0]))
     ]
     row_format = "".join(["{:>" + str(longest_col) + "}" for longest_col in longest_cols])
-    names = ["Name", "Price", "Avail.", "Raiting"] if len(matches[0]) > 2 else ["Name", "Price"]
+    names = ["Matching", "Name", "Price", "Avail.", "Raiting"] if len(matches[0]) > 2 else ["Name", "Price"]
     print(row_format.format(*names))
     for row in matches:
         print(row_format.format(*row))
@@ -137,7 +158,12 @@ def print_matching_result(query: str, matches: list):
     print("+-----------------------------------------------------------------------------+")
 
 
-book_scraper = BookSearchScraper()
-query="Alice in wonderland"
-res_books = book_scraper.collect_search_data(query, search_all_pages=True, extended_info=True)
-print_matching_result(query, res_books)
+book_scraper = BookSearchScraper(cache_update_ts=3)
+
+for i in ["alice in wonderland", "sharp", "bachelor", "the"]:
+    t1 = time.time()
+    res_books = book_scraper.collect_search_data(i, search_all_pages=True, extended_info=False)
+    print_matching_result(i, res_books)
+    print(f"Get main and category urls {time.time()-t1}")
+
+
